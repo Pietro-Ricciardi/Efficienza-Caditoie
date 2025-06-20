cb7fc0c6b296388afe3fae39bb030beca5524268
Pietro Ricciardi <pietro.ricciardi@gmail.com>
2025-06-20 11:21:16 +0200

    Revert "Refine HELP formulas"
    This reverts commit ddef0197d138fe23f3085f72b2a0a5f2520f35cf.

--- src/components/Graphs.jsx
+++ src/components/Graphs.jsx
@@ -17,11 +17,12 @@ import {
   Cell,
   LineChart,
   Line,
-  LabelList,
+  LabelList
 } from 'recharts';
 import Widget from '../Widget';
 import EvolutionTable from './EvolutionTable';
 import SedimentGraphs from './SedimentGraphs';
+import Formula from '../Formula';
 
 export default function Graphs({
   R1,
@@ -48,7 +49,7 @@ export default function Graphs({
   pieRef,
   lineRef,
   evolutionRef,
-  resultsRef,
+  resultsRef
 }) {
   const widgetMap = {
     results: (
@@ -60,31 +61,47 @@ export default function Graphs({
         onDrop={handleDrop}
       >
         <div className="formula-list">
-          <p>R1 = 1 - 0.3 (v - v0) = {R1.toFixed(2)}</p>
           <p>
-            R2 = 1 / (1 + (0.083 * v<sup>1.8</sup>) / (j * L<sup>2/3</sup>)) =
-            {R2.toFixed(2)}
-          </p>
-          <p>Q1* = Q1 × R1 = {Q1_star.toFixed(2)}</p>
-          <p>Q2 = Q - Q1 = {Q2.toFixed(2)}</p>
-          <p>Q2* = Q2 × R2 = {Q2_star.toFixed(2)}</p>
-          <p>E = (Q1* + Q2*) / Q = {E.toFixed(2)}</p>
-          <p>E formula = R1 × E0 + R2 × (1 - E0) = {E_formula.toFixed(2)}</p>
-          <p>τ = ρ g h j = {sedimentData.tau.toFixed(2)}</p>
-          <p>θ = τ / ((ρ_s - ρ) g d) = {sedimentData.theta.toFixed(2)}</p>
-          <p>τ_c = θ_c (ρ_s - ρ) g d = {sedimentData.tauC.toFixed(2)}</p>
-          <p>
-            q_s^MPM = 8 √[g(s - 1) d³] (θ - θ_c)<sup>3/2</sup> ={' '}
-            {sedimentData.bedloadMPM.toExponential(2)}
+            <Formula>{`\\(R_1 = 1 - 0.3\,(v - v_0) = ${R1.toFixed(2)}\\)`}</Formula>
           </p>
           <p>
-            q_s^E = 0.4 √[g(s - 1) d³] (θ - θ_c)<sup>5/2</sup> ={' '}
-            {sedimentData.bedloadEinstein.toExponential(2)}
+            <Formula>{`\\(R_2 = \\frac{1}{1 + \\frac{0.083\\, v^{1.8}}{j\\, L^{2/3}}} = ${R2.toFixed(2)}\\)`}</Formula>
           </p>
-          <p>P = w_s / (κ u_*) = {sedimentData.rouseP.toFixed(2)}</p>
           <p>
-            Q_s = 0.5(0.05 θ^{2.5} + 0.016 θ^{2.1}) √[g(s - 1) d³] ={' '}
-            {sedimentData.totalLoad.toExponential(2)}
+            <Formula>{`\\(Q_1^* = Q_1\\, R_1 = ${Q1_star.toFixed(2)}\\)`}</Formula>
+          </p>
+          <p>
+            <Formula>{`\\(Q_2 = Q - Q_1 = ${Q2.toFixed(2)}\\)`}</Formula>
+          </p>
+          <p>
+            <Formula>{`\\(Q_2^* = Q_2\\, R_2 = ${Q2_star.toFixed(2)}\\)`}</Formula>
+          </p>
+          <p>
+            <Formula>{`\\(E = \\frac{Q_1^* + Q_2^*}{Q} = ${E.toFixed(2)}\\)`}</Formula>
+          </p>
+          <p>
+            <Formula>{`\\(E_{\\text{formula}} = R_1\\, E_0 + R_2\\,(1 - E_0) = ${E_formula.toFixed(2)}\\)`}</Formula>
+          </p>
+          <p>
+            <Formula>{`\\(\\tau = \\rho\\, g\\, h\\, j = ${sedimentData.tau.toFixed(2)}\\)`}</Formula>
+          </p>
+          <p>
+            <Formula>{`\\(\\theta = \\frac{\\tau}{(\\rho_s - \\rho)\\, g\\, d} = ${sedimentData.theta.toFixed(2)}\\)`}</Formula>
+          </p>
+          <p>
+            <Formula>{`\\(\\tau_c = \\theta_c(\\rho_s - \\rho) g d = ${sedimentData.tauC.toFixed(2)}\\)`}</Formula>
+          </p>
+          <p>
+            <Formula>{`\\(q_s^{MPM} = 8\\,\\sqrt{g(s-1)d^3}(\\theta-\\theta_c)^{3/2} = ${sedimentData.bedloadMPM.toExponential(2)}\\)`}</Formula>
+          </p>
+          <p>
+            <Formula>{`\\(q_s^{E} = 0.4\\,\\sqrt{g(s-1)d^3}(\\theta-\\theta_c)^{5/2} = ${sedimentData.bedloadEinstein.toExponential(2)}\\)`}</Formula>
+          </p>
+          <p>
+            <Formula>{`\\(P = \\frac{w_s}{\\kappa u_*} = ${sedimentData.rouseP.toFixed(2)}\\)`}</Formula>
+          </p>
+          <p>
+            <Formula>{`\\(Q_s = 0.5(0.05\\,\\theta^{2.5} + 0.016\\theta^{2.1})\\,\\sqrt{g(s-1)d^3} = ${sedimentData.totalLoad.toExponential(2)}\\)`}</Formula>
           </p>
         </div>
       </Widget>
@@ -130,7 +147,11 @@ export default function Graphs({
             <YAxis domain={[0, 1]} />
             <Tooltip />
             <Bar dataKey="value" fill="#82ca9d">
-              <LabelList dataKey="value" position="top" formatter={(v) => v.toFixed(2)} />
+              <LabelList
+                dataKey="value"
+                position="top"
+                formatter={(v) => v.toFixed(2)}
+              />
             </Bar>
           </BarChart>
         </ResponsiveContainer>
@@ -176,7 +197,11 @@ export default function Graphs({
             <YAxis domain={[0, 1]} />
             <Tooltip />
             <Line type="monotone" dataKey="value" stroke="#8884d8">
-              <LabelList dataKey="value" position="top" formatter={(v) => v.toFixed(2)} />
+              <LabelList
+                dataKey="value"
+                position="top"
+                formatter={(v) => v.toFixed(2)}
+              />
             </Line>
           </LineChart>
         </ResponsiveContainer>
@@ -196,7 +221,11 @@ export default function Graphs({
             <YAxis domain={[0, 1]} />
             <Tooltip />
             <Line type="monotone" dataKey="efficiency" stroke="#ff7300">
-              <LabelList dataKey="efficiency" position="top" formatter={(v) => v.toFixed(2)} />
+              <LabelList
+                dataKey="efficiency"
+                position="top"
+                formatter={(v) => v.toFixed(2)}
+              />
             </Line>
           </LineChart>
         </ResponsiveContainer>
@@ -212,10 +241,12 @@ export default function Graphs({
         <EvolutionTable evolutionData={evolutionData} rangeVar={rangeVar} />
       </Widget>
     ),
-    sediments: <SedimentGraphs params={params} sedimentData={sedimentData} />,
+    sediments: <SedimentGraphs params={params} sedimentData={sedimentData} />
   };
 
-  return <>{widgetOrder.map((w) => (visibleCharts[w] ? widgetMap[w] : null))}</>;
+  return (
+    <>{widgetOrder.map((w) => (visibleCharts[w] ? widgetMap[w] : null))}</>
+  );
 }
 
 Graphs.propTypes = {
@@ -243,5 +274,5 @@ Graphs.propTypes = {
   pieRef: PropTypes.object,
   lineRef: PropTypes.object,
   evolutionRef: PropTypes.object,
-  resultsRef: PropTypes.object,
+  resultsRef: PropTypes.object
 };
